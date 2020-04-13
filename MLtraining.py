a4fa4929067f9f5c0702d5097a6890a4224086aa
VaseSimion <42439265+VaseSimion@users.noreply.github.com>
2020-04-13 20:30:10 +0200

    The machine learning stuff stable

--- MLtraining.py
+++ MLtraining.py
@@ -16,36 +16,62 @@ for row in reader:
 
 input_data = np.array(input_data)
 result = np.array(result)
-training_data = input_data[:1500]
-test_data = input_data[1500:]
-training_results = result[:1500]
-test_results = result[1500:]
+training_data = input_data[:5000]
+test_data = input_data[5000:]
+training_results = result[:5000]
+test_results = result[5000:]
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(126, activation='elu', input_dim=99)) #Dense(output_dim(also hidden wight), input_dim = input_dim)
-model.add(tf.keras.layers.LeakyReLU(alpha = 0.1)) #Activation
+model.add(tf.keras.layers.Dense(252, activation='elu', input_dim=99)) #Dense(output_dim(also hidden wight), input_dim = input_dim)
+model.add(tf.keras.layers.LeakyReLU(alpha = 0.4)) #Activation
 
-model.add(tf.keras.layers.Dropout(0.1, noise_shape=None, seed=None))
-
-model.add(tf.keras.layers.Dense(252, use_bias=True, activation='elu'))
-model.add(tf.keras.layers.LeakyReLU(alpha = 0.1))
+#model.add(tf.keras.layers.Dropout(0.1, noise_shape=None, seed=None))
 
 model.add(tf.keras.layers.Dense(126, activation='elu'))
-model.add(tf.keras.layers.LeakyReLU(alpha = 0.1))
+model.add(tf.keras.layers.LeakyReLU(alpha = 0.4))
+
+model.add(tf.keras.layers.Dense(63, activation='elu'))
+model.add(tf.keras.layers.LeakyReLU(alpha = 0.4))
 
 model.add(tf.keras.layers.Dense(1))
 model.add(tf.keras.layers.Activation('linear'))
 
-model.compile(optimizer='adam', loss='mean_squared_logarithmic_error')
+model.compile(optimizer='adam', loss='mean_absolute_percentage_error')
 
-model.fit(training_data, training_results, epochs=10000)
+#log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+#tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+model.fit(training_data, training_results, epochs=1000)
 
 print("evaluate")
 model.evaluate(test_data, test_results)
 
 print(model.summary())
-value_to_predict = np.array([input_data[90]])
-print("Initially it was {} and predicted was {}".format(result[90]/input_data[90][0],model.predict(np.array([input_data[90]]))/input_data[90][0]))
-print("Initially it was {} and predicted was {}".format(result[1510]/input_data[1510][0],model.predict(np.array([input_data[1510]]))/input_data[1510][0]))
-print("Initially it was {} and predicted was {}".format(result[1520]/input_data[1520][0],model.predict(np.array([input_data[1520]]))/input_data[1520][0]))
-print("Initially it was {} and predicted was {}".format(result[1530]/input_data[1530][0],model.predict(np.array([input_data[1530]]))/input_data[1530][0]))
+check_index = 90
+value = result[check_index]/input_data[check_index][0]
+predicted_value = model.predict(np.array([input_data[check_index]]))/input_data[check_index][0]
+print("Ratio between the value and predicted value is {}, with {} as value and {} as predicted value".format(value/predicted_value,value,predicted_value))
+check_index = 5001
+value = result[check_index]/input_data[check_index][0]
+predicted_value = model.predict(np.array([input_data[check_index]]))/input_data[check_index][0]
+print("Ratio between the value and predicted value is {}, with {} as value and {} as predicted value".format(value/predicted_value,value,predicted_value))
+check_index = 5002
+value = result[check_index]/input_data[check_index][0]
+predicted_value = model.predict(np.array([input_data[check_index]]))/input_data[check_index][0]
+print("Ratio between the value and predicted value is {}, with {} as value and {} as predicted value".format(value/predicted_value,value,predicted_value))
+check_index = 5003
+value = result[check_index]/input_data[check_index][0]
+predicted_value = model.predict(np.array([input_data[check_index]]))/input_data[check_index][0]
+print("Ratio between the value and predicted value is {}, with {} as value and {} as predicted value".format(value/predicted_value,value,predicted_value))
+check_index = 5004
+value = result[check_index]/input_data[check_index][0]
+predicted_value = model.predict(np.array([input_data[check_index]]))/input_data[check_index][0]
+print("Ratio between the value and predicted value is {}, with {} as value and {} as predicted value".format(value/predicted_value,value,predicted_value))
+check_index = 5100
+value = result[check_index]/input_data[check_index][0]
+predicted_value = model.predict(np.array([input_data[check_index]]))/input_data[check_index][0]
+print("Ratio between the value and predicted value is {}, with {} as value and {} as predicted value".format(value/predicted_value,value,predicted_value))
+check_index = 5105
+value = result[check_index]/input_data[check_index][0]
+predicted_value = model.predict(np.array([input_data[check_index]]))/input_data[check_index][0]
+print("Ratio between the value and predicted value is {}, with {} as value and {} as predicted value".format(value/predicted_value,value,predicted_value))
