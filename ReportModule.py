d0c8eb513882b6c646987e25ac3c90130ac6e47a
VaseSimion <42439265+VaseSimion@users.noreply.github.com>
2020-05-21 18:56:49 +0200

    Don't really know why these files were not uploaded last time

--- ReportModule.py
+++ ReportModule.py
@@ -5,7 +5,10 @@ def append_both(stock_performance, report_file):
                       " predicted as price increase and " + str(stock_performance[3])
                       + " increase predicted by the category one with " + str(100*stock_performance[5]) + "%(" +
                       str(100*stock_performance[6]) + "%)" + " as confidence" + "\n")
-    report_file.write(str(yf.Ticker(stock_performance[0]).financials.iloc[[2, 6, 15]]))
+    try:
+        report_file.write(str(yf.Ticker(stock_performance[0]).financials.iloc[[2, 6, 15]]))
+    except:
+        report_file.write("There was some error with getting historical data")
     report_file.write("\n")
     report_file.write("\n")
 
@@ -14,8 +17,10 @@ def append_category(stock_performance, report_file):
                       " predicted as price increase and " + str(stock_performance[3])
                       + " increase predicted by the category one with " + str(100*stock_performance[5]) + "%(" +
                       str(100*stock_performance[6]) + "%)" + " as confidence" + "\n")
-
-    report_file.write(str(yf.Ticker(stock_performance[0]).financials.iloc[[2, 6, 15]]))
+    try:
+        report_file.write(str(yf.Ticker(stock_performance[0]).financials.iloc[[2, 6, 15]]))
+    except:
+        report_file.write("There was some error with getting historical data")
     report_file.write("\n")
     report_file.write("\n")
 
@@ -23,7 +28,9 @@ def append_category(stock_performance, report_file):
 def append_price_prediction(stock_performance, report_file):
     report_file.write(stock_performance[0] + " at " + str(stock_performance[2]) + "$ is predicted by price script with "
                       + str(stock_performance[1]) + " price increase" + "\n")
-
-    report_file.write(str(yf.Ticker(stock_performance[0]).financials.iloc[[2, 6, 15]]))
+    try:
+        report_file.write(str(yf.Ticker(stock_performance[0]).financials.iloc[[2, 6, 15]]))
+    except:
+        report_file.write("There was some error with getting historical data")
     report_file.write("\n")
     report_file.write("\n")
