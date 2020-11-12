4620b7f769473fc93ec8b8e761409d4aedb9d589
VaseSimion <42439265+VaseSimion@users.noreply.github.com>
2020-11-12 21:30:55 +0100

    Added average calculation in the ReportModule.py

--- ReportModule.py
+++ ReportModule.py
@@ -42,6 +42,8 @@ def get_list_of_3_weeks_ago():
 def return_report_from_3_weeks_ago():
     zile_in_trecut = 1  # cate zile o trecut de vineri pana acum:  daca e luni ii 3
     list_of_stocks = []
+    sum_of_values = 0
+    stocks = 0
     for stock in ["^GSPC"] + get_list_of_3_weeks_ago():
         try:
             weekly = yf.download(tickers=stock, interval="1d", period="6mo")
@@ -49,10 +51,13 @@ def return_report_from_3_weeks_ago():
             closing.reverse()
             today = weekly.loc[str(date.today() + timedelta(-zile_in_trecut))]  # last Friday is considered today
             print(str(date.today() + timedelta(- zile_in_trecut)))
-            old_day = weekly.loc[str(date.today() + timedelta(-18 - zile_in_trecut))] # 3 weeks ago monday is old day
+            old_day = weekly.loc[str(date.today() + timedelta(-18 - zile_in_trecut))]  # 3 weeks ago monday is old day
             print(str(date.today() + timedelta(-18 - zile_in_trecut)))
             print(stock, str(today["Close"] / old_day["Open"]))
             list_of_stocks.append([stock, old_day["Open"], today["Close"] / old_day["Open"]])
+            if stock != "^GSPC":
+                sum_of_values = sum_of_values + today["Close"] / old_day["Open"]
+                stocks += 1
         except:
             print("error")
     string_to_output = ""
@@ -60,4 +65,5 @@ def return_report_from_3_weeks_ago():
         string_to_output += element[0] + " bought at " + str(round(element[1], 2)) + " had an increase of " + \
                             str(round(100*(element[2] - 1), 2)) + "%\n"
     # the output of this function is a string so it can be put in the mail module easily
+    string_to_output += "Average win for this period was " + str(round(100 * (sum_of_values / stocks - 1), 2)) + "%\n"
     return string_to_output
